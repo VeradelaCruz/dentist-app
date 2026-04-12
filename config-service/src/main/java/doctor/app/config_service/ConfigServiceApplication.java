40f485cd691b9664b491b54457c6146a468dfd00
VeradelaCruz <maidanabru@yahoo.com.ar>
2026-04-12 18:43:30 +0200

    Change configutarion in config-service and docker compose file

--- config-service/src/main/java/doctor/app/config_service/ConfigServiceApplication.java
+++ config-service/src/main/java/doctor/app/config_service/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package doctor.app.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
