78a386236bbe50182feb03b9173e0cbce5032338
VeradelaCruz <maidanabru@yahoo.com.ar>
2026-04-15 08:34:23 +0200

    change: secutity config enpoints in patien-service

--- patient-service/src/main/java/doctor/app/patient_service/config/SecurityConfig.java
+++ patient-service/src/main/java/doctor/app/patient_service/config/SecurityConfig.java
@@ -5,19 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 				.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/patients/**").permitAll()
-						.requestMatchers("/actuator/health", "/actuator/info").permitAll()
-						.anyRequest().authenticated());
+						.requestMatchers("/actuator/**").permitAll()
+						.anyRequest().permitAll()
+				)
+				.httpBasic(httpBasic -> httpBasic.disable())
+				.formLogin(form -> form.disable());
+
 		return http.build();
 	}
 }
